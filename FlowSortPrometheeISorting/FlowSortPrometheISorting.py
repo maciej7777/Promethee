154651b81d76b30fc0c2436c5ca2c76532fadd38
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2016-09-24 13:16:07 +0200

    Pierszy commit dla FlowSort Promethe I

---
+++ FlowSortPrometheeISorting/FlowSortPrometheISorting.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from docopt import docopt
+
+
+def sortWithLimitingProfiles():
+  
+
+
+def sortWithCentralProfiles():
+  
+
+
+def main():
+  
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  sys.exit(main())
